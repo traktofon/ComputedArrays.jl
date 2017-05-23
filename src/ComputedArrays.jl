95595914cd73ea4ec9be3b570d492fbc505dd94f
Frank Otto <traktofon@gmail.com>
2017-05-23 16:08:56 +0800

    Add order argument to constructor.

--- src/ComputedArrays.jl
+++ src/ComputedArrays.jl
@@ -3,37 +3,40 @@ module ComputedArrays
 # A = ComputedArray(fn, xs, ys, zs)
 # => A[i,j,k] = fn(xs[i], ys[j], zs[k])
 
-# A = ComputedArray(fn, xs, ys, zs, order=(2,3,1))
+# A = ComputedArray(fn, xs, ys, zs, order=[2,3,1])
 # => A[j,k,i] = fn(xs[i], ys[j], zs[k])
 # => A[i,j,k] = fn(xs[k], ys[i], zs[j])
 
 import Base: size, getindex
 export ComputedArray, ComputedVector, ComputedMatrix
 
-struct ComputedArray{F,C,T,N} <: AbstractArray{T,N}
+struct ComputedArray{F,C,T,N} <: DenseArray{T,N}
    fn     :: F
    coords :: C
+   perm   :: NTuple{N,Int}
 end
 
 const ComputedVector{F,C,T} = ComputedArray{F,C,T,1}
 const ComputedMatrix{F,C,T} = ComputedArray{F,C,T,2}
 
-function ComputedArray(fn, coords...)
+function ComputedArray(fn, coords...; order=1:length(coords))
    N = length(coords)
    argtypes = map(eltype, coords)
    T = Core.Inference.return_type(fn, argtypes)
-   ComputedArray{typeof(fn), typeof(coords), T, N}(fn, coords)
+   @assert length(order)==N "order must have $N elements"
+   @assert isperm(order) "order=$order is not a permutation"
+   perm = tuple( invperm(order)... )
+   ComputedArray{typeof(fn), typeof(coords), T, N}(fn, coords, perm)
 end
 
 size(A::ComputedArray) = map(length, A.coords)
 
 getindex(A::ComputedVector, i::Int) = A.fn(A.coords[1][i])
-getindex(A::ComputedMatrix, i::Int, j::Int) = A.fn(A.coords[1][i], A.coords[2][j])
 
 @generated function getindex(A::ComputedArray{F,C,T,N}, I::Vararg{Int,N}) where {F,C,T,N}
    ex = :( A.fn() )
    for d = 1:N
-      push!(ex.args, :( A.coords[$d][I[$d]] ))
+      push!(ex.args, :( A.coords[$d][I[A.perm[$d]]] ))
    end
    return ex
 end
